288b48cc118ceae9911faa0c757854dd82e8f000
PetitPrinc3 <arthur.reppelin@gmail.com>
2024-05-03 16:55:09 -0400

    Banner fix. Exposed API keys.

--- lib/secrets.py
+++ lib/secrets.py
@@ -1,4 +1,4 @@
 # Secrets for SNCF-Sucks
 
-sncf_secret = '' # Return API Key
-airfrance_secret = '' # Return API Key
+sncf_secret = '4465b31a-d443-4f12-846d-4cc961bdaf83' # Return API Key
+airfrance_secret = 'ftnjaxs6y9mpsajjxfrp36jx' # Return API Key
